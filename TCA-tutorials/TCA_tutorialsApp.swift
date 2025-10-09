19b97806e09c54d3d4ba8d9dbb32747c37438400
hotaka <s1f102103864@iniad.org>
2025-10-09 11:17:53 +0900

    first commit

---
+++ TCA-tutorials/TCA_tutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_tutorialsApp.swift
+//  TCA-tutorials
+//
+//  Created by Hotaka Kanazawa on 2025/10/09.
+//
+
+import SwiftUI
+
+@main
+struct TCA_tutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
